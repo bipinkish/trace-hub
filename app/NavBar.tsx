b9fe6b268ca557604c2c810cb2806c798bd3ef29
Bipin Kishore N <bipinkishore01@gmail.com>
2025-07-05 19:54:30 +0530

    added navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Link from "next/link";
+import { FaBug } from "react-icons/fa6";
+
+const NavBar = () => {
+  const links = [
+    { href: "/dashboard", label: "Dashboard" },
+    { href: "/issues", label: "Issues" },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <FaBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Lato } from "next/font/google";
 import "./globals.css";
+import NavBar from "./NavBar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const lato = Lato({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
@@ -24,10 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${lato.className} antialiased`}>
+        <NavBar />
+        <main>{children}</main>
       </body>
     </html>
   );
