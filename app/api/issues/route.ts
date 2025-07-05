1f5e0b9ac00e1350d0cc3e6bcbe77c0fd52323df
Bipin Kishore N <bipinkishore01@gmail.com>
2025-07-05 21:04:16 +0530

    create an issue api

---
+++ app/api/issues/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import { title } from "process";
+import z from "zod";
+import { prisma } from "@/prisma/client";
+
+const createIssueSchema = z.object({
+  title: z
+    .string()
+    .min(1, "Title is required")
+    .max(255, "Title must be less than 255 characters"),
+  description: z.string().min(1, "Description is required"),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = createIssueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, {
+      status: 400,
+    });
+
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: validation.data.title,
+      description: validation.data.description,
+    },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
+}
