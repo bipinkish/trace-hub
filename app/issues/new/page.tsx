8a825f589848ee7e1705b1dd6b58f3f9c86d921a
Bipin Kishore N <bipinkishore01@gmail.com>
2025-07-06 08:02:26 +0530

    handle error

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,19 +1,57 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { Box, Button, TextField } from "@radix-ui/themes";
+import { Box, Button, Callout, TextField } from "@radix-ui/themes";
 import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
 });
 
-const NewIssuePage = () => (
-  <Box className="max-w-xl space-y-3">
-    <TextField.Root placeholder="Title"></TextField.Root>{" "}
-    <SimpleMDE placeholder="Description" />
-    <Button>Submit New Issue</Button>
-  </Box>
-);
+const NewIssuePage = () => {
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+  console.log(register("title"));
+  return (
+    <Box className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-4">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
 
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("Failed to create issue. Please try again.");
+          }
+        })}
+      >
+        <TextField.Root placeholder="Title" {...register("title")} />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE {...field} placeholder="Description" />
+          )}
+        />
+        <Button>Submit New Issue</Button>
+      </form>
+    </Box>
+  );
+};
 export default NewIssuePage;
