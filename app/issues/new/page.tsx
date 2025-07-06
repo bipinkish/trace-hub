f053bcf1cbaba086255cb98d2c44db6a59f82c60
Bipin Kishore N <bipinkishore01@gmail.com>
2025-07-06 09:16:33 +0530

    added spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,25 +1,41 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { Box, Button, Callout, TextField } from "@radix-ui/themes";
+import {
+  Box,
+  Button,
+  Callout,
+  Spinner,
+  Text,
+  TextField,
+} from "@radix-ui/themes";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import z from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
 });
 
 const NewIssuePage = () => {
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [error, setError] = useState<string | null>(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const router = useRouter();
   console.log(register("title"));
   return (
@@ -34,14 +50,18 @@ const NewIssuePage = () => {
         className="space-y-3"
         onSubmit={handleSubmit(async (data) => {
           try {
+            setIsSubmitting(true);
             await axios.post("/api/issues", data);
             router.push("/issues");
           } catch (error) {
+            setIsSubmitting(false);
             setError("Failed to create issue. Please try again.");
           }
         })}
       >
         <TextField.Root placeholder="Title" {...register("title")} />
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+
         <Controller
           name="description"
           control={control}
@@ -49,7 +69,10 @@ const NewIssuePage = () => {
             <SimpleMDE {...field} placeholder="Description" />
           )}
         />
-        <Button>Submit New Issue</Button>
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        <Button disabled={isSubmitting} type="submit" size="2">
+          Submit New Issue {isSubmitting && <Spinner size="2" />}
+        </Button>
       </form>
     </Box>
   );
