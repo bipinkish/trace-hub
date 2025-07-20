a4981408e67a702d4b0e987f7806bf66c197d6b6
Bipin Kishore N <bipinkishore01@gmail.com>
2025-07-20 17:36:26 +0530

    refactored

---
+++ app/components/Navbar/AuthStatus.tsx
@@ -0,0 +1,40 @@
+import { Avatar, Box, DropdownMenu, Text } from "@radix-ui/themes";
+import { getServerSession } from "next-auth";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+  if (status === "loading") return null;
+  if (status === "unauthenticated")
+    return (
+      <Link className="nav-link" href={"/api/auth/signin"}>
+        Login
+      </Link>
+    );
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user?.image!}
+            fallback="?"
+            size={"2"}
+            radius="full"
+            className="cursor-pointer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size={"2"}>{session!.user?.email!}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href={"/api/auth/signout"}>Logout</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
+  );
+};
+
+export default AuthStatus;
---
+++ app/components/Navbar/NavLinks.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import classNames from "classnames";
+import { usePathname } from "next/navigation";
+
+const NavLinks = () => {
+  const links = [
+    { href: "/dashboard", label: "Dashboard" },
+    { href: "/issues/list", label: "Issues" },
+  ];
+  const currentPath = usePathname();
+  return (
+    <ul className="flex space-x-6">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={classNames({
+              "nav-link": true,
+              "!text-zinc-900": link.href === currentPath,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default NavLinks;
